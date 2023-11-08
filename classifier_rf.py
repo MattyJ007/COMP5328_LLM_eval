7863a4937395a2f2a2fe21aa56a8d76b8df339f3
barcher13 <46035422+barcher13@users.noreply.github.com>
2023-11-08 15:57:46 +1100

    Updated loadData and rf

--- classifier_rf.py
+++ classifier_rf.py
@@ -9,11 +9,12 @@ from loadData import getData
 
 def classifyRf(filename, transition_matrix, count=2): #count should be 10 according to 2.1.1
 
+    print("Running RF on", filename)
+
     xtr_val, str_val, xts, yts = getData(filename)
 
     # Preprocessing
     num_classes = len(np.unique(str_val))
-    xtr_val = xtr_val.reshape(xtr_val.shape[0], -1).astype('float32') / 255
     str_val_categorical = to_categorical(str_val, num_classes)
     
     # Define the parameter grid for hyperparameter tuning
@@ -32,13 +33,20 @@ def classifyRf(filename, transition_matrix, count=2): #count should be 10 accord
 
     best_rf = grid_search.best_estimator_
     best_score = grid_search.best_score_
+
+    str_val_pred = best_rf.predict(xtr_val)
+    # Construct confusion matrix using predicted and noisy labels
+    conf_matrix = confusion_matrix(np.argmax(str_val_categorical, axis=1), str_val_pred)
+    # Normalize the confusion matrix to estimate the transition matrix
+    estimated_transition_matrix = normalize(conf_matrix, axis=1, norm='l1')
+    print("Estimated Transition Matrix: \n", estimated_transition_matrix)
+    print("Actual Transition Matrix: \n", transition_matrix)
     
     print("RF Best Validation Accuracy: ", best_score)
     print("Best Parameters: ", grid_search.best_params_)
 
     best_rf.fit(xtr_val, np.argmax(str_val_categorical, axis=1))
     
-    xts = xts.reshape(xts.shape[0], -1).astype('float32') / 255
     yts_pred = best_rf.predict(xts)
     test_accuracy = accuracy_score(yts, yts_pred)
     print("RF Test Accuracy: ", test_accuracy)
--- loadData.py
+++ loadData.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.color import rgb2gray
 
 # Default to CIFAR for testing
 def getData(file_path):
@@ -7,4 +8,19 @@ def getData(file_path):
   str_val = dataset['Str']
   xts = dataset['Xts']
   yts = dataset['Yts']
+
+  #convert to greyscale
+  if xtr_val.shape[-1] == 3:
+        print("test")
+        xtr_val = rgb2gray(xtr_val)
+        xts = rgb2gray(xts)
+
+  #flatten the image into 1D array
+  xtr_val = xtr_val.reshape(xtr_val.shape[0], -1)
+  xts = xts.reshape(xts.shape[0], -1)
+
+  #normalize the data
+  xtr_val = xtr_val.astype('float32') / 255.
+  xts = xts.astype('float32') / 255.
+
   return xtr_val, str_val, xts, yts
